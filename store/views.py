012ff1e9b2b85f9e2cb61c0f6115410d6b91f947
areenjawabrehcoretech <areen.jawabreh@coretechpal.com>
2026-06-06 13:55:20 +0300

    add html code for home and base

--- store/views.py
+++ store/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Product, Category
 
+def categories(request):
+    return{
+    'categories': Category.objects.all()
+    }
 def all_products(request):
     products = Product.objects.all()
-    categories = Category.objects.all()
     return render(request, 'store/home.html', {'products': products})
